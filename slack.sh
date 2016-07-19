56789d03a8c56d9da42e4cfb698de42f39037a86
Jaehoon Choi <plaintext@andromedarabbit.net>
2016-07-19 22:39:47 +0900

    First commit

---
+++ slack.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Post to Slack channel on new deployment
+# https://api.slack.com/incoming-webhooks
+#
+# You can either add those here, or configure them on the environment tab of your
+# project settings.
+SLACK_WEBHOOK_TOKEN=${SLACK_WEBHOOK_TOKEN:?'You need to configure the SLACK_WEBHOOK_TOKEN environment variable!'}
+SLACK_BOT_NAME=${SLACK_BOT_NAME:="Codeship Bot"}
+SLACK_ICON_URL=${SLACK_ICON_URL:="https://d1089v03p3mzyq.cloudfront.net/assets/website/logo-dark-90f893a2645c98929b358b2f93fa614b.png"}
+
+SLACK_MESSAGE="*${CI_REPO_NAME}* is deployed!"
+
+curl -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"username": "'"${SLACK_BOT_NAME}"'",
+  "text": "'"${SLACK_MESSAGE}"'",
+  "attachments": [{ "color": "good", "text": "Docker image was deployed to '"${DEPLOY_TO}"':'"${CI_BRANCH}"'.'"${CI_COMMIT_ID}"' from '"${CI_NAME}"'" }],
+  "mrkdwn": true,
+  "icon_url": "'"${SLACK_ICON_URL}"'"}' \
+  https://hooks.slack.com/services/$SLACK_WEBHOOK_TOKEN
